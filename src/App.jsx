84119b9b031e4e392bf4e554aafe3b23b6e77b49
Syed Moaaz Kamal <workmoaaz@gmail.com>
2026-04-20 13:27:15 +0500

    checking

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,3 @@
-// import Navbar from './components/Navbar';
-// import Hero from './components/Hero';
-// import Features from './components/Features';
-// import WhyGymfinity from './components/WhyGymfinity';
-// import Pricing from './components/Pricing';
-// import Contact from './components/Contact';
-// import Footer from './components/Footer';
-
-// function App() {
-//     return (
-//         <div className="min-h-screen bg-white font-sans antialiased">
-//             <Navbar />
-//             <main>
-//                 <Hero />
-//                 <Features />
-//                 <WhyGymfinity />
-//                 <Pricing />
-//                 <Contact />
-//             </main>
-//             <Footer />
-//         </div>
-//     );
-// }
-
-// export default App;
-
-
-
 import { Routes, Route } from "react-router-dom";
 
 import Navbar from './components/Navbar';
